b1216cf5f26c5ed06a60a7d80376ac06a34e3a96
mrSquidee <mr.squidy78@gmail.com>
2019-04-02 15:25:07 -0700

    added initial code (not tested) v2

--- createTransations.py
+++ createTransations.py
@@ -4,6 +4,6 @@ from random import randint
 def writeTransactions(file):
     with open(file, 'w') as transations:
         transationsStr = ''
-        for i in range(randint(0,100)):
+        for _ in range(randint(0,100)):
             transationsStr += sha256(str(randint(0,10000)).encode("UTF-8")).hexdigest() + ' -{' + str(randint(0,10)) + '}> ' + sha256(str(randint(0,10000)).encode("UTF-8")).hexdigest() + '\n'
         transations.write(transationsStr)
